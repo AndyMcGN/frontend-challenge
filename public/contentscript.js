1578645c8afa939822182c41339eb13ab18806e4
AndyMcGN <66390686+AndyMcGN@users.noreply.github.com>
2022-12-18 18:00:22 +0100

    Completed Assignment
    - extension like el created with click effect
    - tests written
    - history table added
    - git repo description added

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,9 +1,45 @@
 console.log('Contentscript injected');
+let currentlyShowingInfo = false;
 
-// let element = ...;
+// create two views of the extension component (default and details)
+const extraInfo = createExtraInfoEl();
+const budgetOverview = createbudgetOverview();
 
-// ...
+// create extension el and append to DOM
+let extensionEl = document.createElement('div');
+extensionEl.classList.add('budget-div');
+extensionEl.addEventListener('click', toggleExtraInfo);
+extensionEl.appendChild(budgetOverview);
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
+const parentElement = document.querySelector(
+  '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5',
+);
+parentElement.appendChild(extensionEl);
+
+function createbudgetOverview() {
+  const budgetOverview = document.createElement('div');
+  const logo = document.createElement('img');
+  logo.src = './images/favicon-16x16.png';
+  logo.alt = 'eco.mio logo';
+  budgetOverview.appendChild(logo);
+
+  const p = document.createElement('p');
+  const budget = document.body.textContent.match(/\d+\s?€/);
+  p.innerText = `Budget-to-Beat: ${budget}`;
+
+  budgetOverview.appendChild(p);
+  return budgetOverview;
+}
+
+function createExtraInfoEl() {
+  const extraInfo = document.createElement('div');
+  extraInfo.innerText = `Some more cool info:  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
+
+  return extraInfo;
+}
+
+function toggleExtraInfo() {
+  const childToShow = currentlyShowingInfo ? budgetOverview : extraInfo;
+  document.querySelector('.budget-div').replaceChildren(childToShow);
+  currentlyShowingInfo = !currentlyShowingInfo;
+}
